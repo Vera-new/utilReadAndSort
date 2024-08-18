97b47952a8dd49ab6a6dabe01816f835d9664991
Vera-new <56616618+Vera-new@users.noreply.github.com>
2024-08-18 16:11:14 +0700

    first loading

---
+++ src/main/java/com/company/Main/MyInt.java
@@ -0,0 +1,101 @@
+package com.company.Main;
+// класс для работы с целыми цифрами.
+// Поля: имя файла, количество строк (цифр), минимальное число, максимльное число, сумма чисел и среднее значение.
+public class MyInt {
+    private String fileName;
+    private long strCountInt;
+    private long minInt;
+    private long maxInt;
+    private long sumInt;
+    private static double midlInt;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getStrCountInt() {
+        return strCountInt;
+    }
+
+    public void setStrCountInt(long strCountInt) {
+        this.strCountInt = strCountInt;
+    }
+
+    public long getMinInt(String arg) {
+        if (minInt > Long.parseLong(arg)){
+            minInt = Long.parseLong(arg);
+        }
+        return minInt;
+    }
+
+    public void setMinInt(long minInt) {
+        this.minInt = minInt;
+    }
+
+    public long getMaxInt(String arg) {
+        if (maxInt < Long.parseLong(arg)){
+            maxInt = Long.parseLong(arg);
+        }
+        return maxInt;
+    }
+
+    public void setMaxInt(long maxInt) {
+        this.maxInt = maxInt;
+    }
+
+    public long getSumInt() {
+        return sumInt;
+    }
+
+    public void setSumInt(long sumInt) {
+        this.sumInt = sumInt;
+    }
+
+    public double getMidlInt(String arg) {
+        midlInt = (double)sumInt/strCountInt;
+        return midlInt;
+    }
+
+    public void setMidlInt(double midlInt) {
+        this.midlInt = midlInt;
+    }
+
+    public MyInt(String fileName, long strCountInt, long minInt, long maxInt, long sumInt, double midlInt) {
+        this.fileName = fileName;
+        this.strCountInt = strCountInt;
+        this.minInt = minInt;
+        this.maxInt = maxInt;
+        this.sumInt = sumInt;
+        this.midlInt = midlInt;
+    }
+
+    // метод для вывода полной информации о файле
+    @Override
+    public String toString(){
+        return "Информация о файле: " + fileName + "\n" +
+                "Количество строк в файле: " + strCountInt + "\n" +
+                "Минимальное  значение: " + minInt + "\n" +
+                "Максимальное  значение: " + maxInt + "\n" +
+                "Сумма всех чисел: " + sumInt + "\n" +
+                "Среднее значение: " + midlInt + "\n";
+    }
+
+    // метод для вывода краткой информации о файле
+    public String toStringShort(){
+        return "Информация о файле: " + fileName + "\n" +
+                "Количество строк в файле: " + strCountInt + "\n";
+    }
+
+    // метод сбора статистики о файле
+    public void addInt(String arg){
+        setStrCountInt(getStrCountInt()+1);
+        setSumInt(getSumInt()+Long.parseLong(arg));
+        setMinInt(getMinInt(arg));
+        setMaxInt(getMaxInt(arg));
+        setMidlInt(getMidlInt(arg));
+    }
+}
---
+++ src/main/java/com/company/Main/MyString.java
@@ -0,0 +1,76 @@
+package com.company.Main;
+// класс для работы с строками.
+// Поля: имя файла, количество строк, минимальная длина строки и максимльная длина.
+public class MyString {
+    private String fileName;
+    private long strCountString;
+    private long minLengthStr;
+    private long maxLengthStr;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public long getStrCountString() {
+        return strCountString;
+    }
+
+    public void setStrCountString(long strCountString) {
+        this.strCountString = strCountString;
+    }
+
+    public long getMinLengthStr(String line) {
+        if (minLengthStr > line.length()) {
+            minLengthStr = line.length();
+        }
+        return minLengthStr;
+    }
+
+    public void setMinLengthStr(long minLengthStr) {
+        this.minLengthStr = minLengthStr;
+    }
+
+    public long getMaxLengthStr(String line) {
+        if (maxLengthStr < line.length()) {
+            maxLengthStr = line.length();
+        }
+        return maxLengthStr;
+    }
+
+    public void setMaxLengthStr(long maxLengthStr) {
+        this.maxLengthStr = maxLengthStr;
+    }
+
+    public MyString(String fileName, long strCountString, long minLengthStr, long maxLengthStr) {
+        this.fileName = fileName;
+        this.strCountString = strCountString;
+        this.minLengthStr = minLengthStr;
+        this.maxLengthStr = maxLengthStr;
+    }
+
+    // метод сбора статистики о файле
+    public void addString (String arg) {
+        setStrCountString(getStrCountString()+1);
+        setMinLengthStr(getMinLengthStr(arg));
+        setMaxLengthStr(getMaxLengthStr(arg));
+    }
+
+    // переопределённый метод вывода полной информации о файле
+    @Override
+    public String toString(){
+        return "Информация о файле: " + fileName + "\n" +
+                "Количество строк в файле: " + strCountString + "\n" +
+                "Минимальная длина строки: " + minLengthStr + "\n" +
+                "Максимальная длина строки: " + maxLengthStr + "\n" ;
+    }
+
+    // метод вывода сокращенной информации о файле
+    public String toStringShort(){
+        return "Информация о файле: " + fileName + "\n" +
+                "Количество строк в файле: " + strCountString + "\n";
+    }
+}
